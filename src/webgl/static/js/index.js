97d66c32bc9c8e8e9cbba351338a8f9b5563dd1a
Zachary Stewart <zstewar1@umbc.edu>
2014-10-26 22:54:07 -0400

    add beginnings of textured obj models

--- src/webgl/static/js/index.js
+++ src/webgl/static/js/index.js
@@ -9,87 +9,92 @@ var windowHalfX = window.innerWidth / 2;
 var windowHalfY = window.innerHeight / 2;
 
 $(document).ready(function() {
-  init();
-  animate();
+    init();
+    animate();
 });
 
 
 function init() {
-	camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 2000);
-	camera.position.z = 50;
+    camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 2000);
+    camera.position.z = 50;
 
-	// scene
+    // scene
 
-	scene = new THREE.Scene();
+    scene = new THREE.Scene();
 
-	var ambient = new THREE.AmbientLight(0x101030);
-	scene.add(ambient);
+    var ambient = new THREE.AmbientLight(0x101030);
+    scene.add(ambient);
 
-	var directionalLight = new THREE.DirectionalLight(0xffeedd);
-	directionalLight.position.set(0, 0, 1);
-	scene.add(directionalLight);
+    var directionalLight = new THREE.DirectionalLight(0xffeedd);
+    directionalLight.position.set(0, 0, 1);
+    scene.add(directionalLight);
 
-	// texture
+    // model
 
-	var manager = new THREE.LoadingManager();
-	manager.onProgress = function (item, loaded, total) {
-		console.log(item, loaded, total);
-	};
+    var onProgress = function(xhr) {
+        if(xhr.lengthComputable) {
+            var percentComplete = xhr.loaded / xhr.total * 100;
+            console.log(Math.round(percentComplete, 2) + '% downloaded');
+        }
+    };
 
-	// model
+    var onError = function(xhr) {
+    };
 
-	var loader = new THREE.OBJLoader(manager);
-	loader.load('models/g1.obj', function (object) {
-    ship = object;
-		scene.add(object);
-	});
+    THREE.Loader.Handlers.add(/\.dds$/i, new THREE.DDSLoader());
 
-	//
+    var loader = new THREE.OBJMTLLoader();
+    loader.load('models/ev_venator.obj', 'models/ev_venator.mtl', function(object) {
+        scene.add(object);
+        ship = object;
+    }, onProgress, onError);
 
-	renderer = new THREE.WebGLRenderer({canvas: $("#main-canvas").get(0)});
-	renderer.setSize(window.innerWidth, window.innerHeight);
+    //
 
-	$(document).mousemove(onDocumentMouseMove);
+    renderer = new THREE.WebGLRenderer({canvas: $("#main-canvas").get(0)});
+    renderer.setSize(window.innerWidth, window.innerHeight);
 
-	//
+    $(document).mousemove(onDocumentMouseMove);
 
-	$(window).resize(onWindowResize);
+    //
 
-  $("#center-btn").click(function() { spin = !spin; });
+    $(window).resize(onWindowResize);
+
+    $("#center-btn").click(function() { spin = !spin; });
 }
 
 function onWindowResize() {
-	windowHalfX = window.innerWidth / 2;
-	windowHalfY = window.innerHeight / 2;
+    windowHalfX = window.innerWidth / 2;
+    windowHalfY = window.innerHeight / 2;
 
-	camera.aspect = window.innerWidth / window.innerHeight;
-	camera.updateProjectionMatrix();
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
 
-	renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.setSize(window.innerWidth, window.innerHeight);
 }
 
 function onDocumentMouseMove(event) {
-	mouseX = (event.clientX - windowHalfX) / 2;
-	mouseY = (event.clientY - windowHalfY) / 2;
+    mouseX = (event.clientX - windowHalfX) / 2;
+    mouseY = (event.clientY - windowHalfY) / 2;
 }
 
 //
 
 function animate() {
-	requestAnimationFrame(animate);
-	render();
+    requestAnimationFrame(animate);
+    render();
 }
 
 function render() {
-	camera.position.x += (mouseX - camera.position.x) * .05;
-	camera.position.y += (-mouseY - camera.position.y) * .05;
+    camera.position.x += (mouseX - camera.position.x) * .05;
+    camera.position.y += (-mouseY - camera.position.y) * .05;
 
-	camera.lookAt(scene.position);
+    camera.lookAt(scene.position);
 
-  if(spin);
-    //ship.rotation.y += .01;
+    if(spin)
+        ship.rotation.y += .01;
 
-	renderer.render(scene, camera);
+    renderer.render(scene, camera);
 }
 
 function registerWithServer() {
