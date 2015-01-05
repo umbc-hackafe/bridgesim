34aa54badc97967b28101070a48b68c4b68b9847
Dylan Whichard <dylan@whichard.com>
2015-01-04 23:04:17 -0500

    Clean up old code in src

--- src/util/3dutils.h
+++
@@ -1,9 +0,0 @@
-#ifndef _3DUTILS_H
-#define _3DUTILS_H
-
-//NOTE: Expects triples for all three.
-//Returns quadruples
-float * CalculateTangentSpace( int Triangles, int VertexCount, int * Indices, float * verts, float * normals, float * texs );
-
-#endif
-
